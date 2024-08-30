96d0877e15efec97df64ff208b999801be7ca50b
lucastso10 <lucastso10@gmail.com>
2024-08-29 21:21:05 -0300

    seleciona o dispositivo e busca a queue de gráficos

--- shell.nix
+++ shell.nix
@@ -6,5 +6,15 @@ pkgs.mkShell {
     rustfmt       # Formatter
     cargo
     rustc
+    vulkan-tools
+    vulkan-loader
+    vulkan-validation-layers
+    cmake
+    ninja
+    python3
   ];
+
+  LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib:${pkgs.vulkan-validation-layers}/lib";
+  VULKAN_SDK = "${pkgs.vulkan-headers}";
+  VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
 }
