4ee875d042d92fe7349cd0e033ed22b388de0ea6
lucastso10 <lucastso10@gmail.com>
2024-09-05 01:25:12 -0300

    abre uma janela com winit e comecei a fazer alocação de buffers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,72 @@
-use std::fmt::Debug;
+use std::sync::Arc;
+
+// criando uma janela e administrando a janela
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
 
 use vulkano::VulkanLibrary;
+
+// criando uma instance do Vulkan https://vulkano.rs/02-initialization/01-initialization.html
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::device::QueueFlags;
-use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo};
+
+// criando abstração por cima da GPU e escolhendo suas queues https://vulkano.rs/02-initialization/02-device-creation.html
+use vulkano::device::{Device, DeviceCreateInfo, QueueCreateInfo, QueueFlags};
+
+// tipos de buffer https://vulkano.rs/03-buffer-creation/01-buffer-creation.html
+use vulkano::buffer::{Buffer, BufferCreateInfo, BufferUsage};
+
+// alocador de memoria (criação de buffer) https://vulkano.rs/03-buffer-creation/01-buffer-creation.html
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() {
+
+    // event loop da janela (winit)
+    let event_loop = EventLoop::new().expect("failed to create an event_loop");
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
 
     // cria procura se existe vulkan no computador e puxa as bibliotecas necessárias
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
@@ -52,4 +113,37 @@ fn main() {
     // valor do vetor
     let queue = queues.next().unwrap();
 
+    // escolhendo o alocador de memória que vai ser utilizado para criar e administrar os buffers
+    //
+    // TODO: aqui está como padrão mas deveriamos dar uma olhada e ver qual seria o melhor
+    // para a nossa aplicação
+    let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
+
+
+    // criando buffer
+    //
+    // nesse caso colocamos o valor 12 no buffer 
+    // mas pode ser qualquer valor de qualquer tipo
+    //
+    // Note: In a real application you shouldn't create buffers with only 4 bytes of data.
+    // Although buffers aren't expensive, you should try to group as
+    // much related data as you can in the same buffer.
+    let data: i32 = 12;
+    let buffer = Buffer::from_data(
+        memory_allocator.clone(),
+        BufferCreateInfo {
+            usage: BufferUsage::UNIFORM_BUFFER,
+            ..Default::default()
+        },
+        AllocationCreateInfo {
+            memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+            ..Default::default()
+        },
+        data,
+    )
+    .expect("failed to create buffer");
+
+    // criar a janela e rodar o event loop (no momento vazio)
+    let _ = event_loop.run_app(&mut app);    
 }
